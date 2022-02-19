b341c9cb6578f32eeddc3d83f267094ad3cfbe99
jake ramsey <jacobramsey31@gmail.com>
2022-02-19 13:31:40 -0500

    User login,Register,and some restful endpoints,assuming form data as per Twains response for login

---
+++ BrainCellsP2/src/main/java/com/revature/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.revature.repositories;
+
+import com.revature.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByuserName(String username);
+
+
+}
---
+++ BrainCellsP2/src/main/java/com/revature/services/UserService.java
@@ -0,0 +1,42 @@
+package com.revature.services;
+
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+    public UserService(UserRepository userRepository){
+    this.userRepository=userRepository;
+    }
+
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User createUser(User u){
+        return userRepository.save(u);
+
+    }
+    public User loginUser(String username,String password){
+        
+        if(userRepository.findByuserName(username).getPassword().equals(password)){
+
+            return userRepository.findByuserName(username);
+        }
+        else{
+
+            return null;
+        }
+    }
+    public Optional<User> getById(int Id){
+       return userRepository.findById(Id);
+    }
+
+}
+
