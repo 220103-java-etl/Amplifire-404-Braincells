525715ee3b498d4ca20115c3621e3e3b273e2024
Michael Lee <mlee9715@outlook.com>
2022-02-21 14:45:04 -0600

    adding customer repor/serv/controller

--- BrainCellsP2/src/main/java/com/revature/services/AgentService.java
+++ BrainCellsP2/src/main/java/com/revature/services/AgentService.java
@@ -1,7 +1,8 @@
 package com.revature.services;
 
 
-import com.revature.models.User;
+import com.revature.models.Agent;
+import java.util.Optional;
 import com.revature.repositories.AgentRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +16,17 @@ public class AgentService {
     }
 
 
-    public List<User> getAllAgents(){
+    public List<Agent> getAllAgents(){
         return agentRepository.findAll();
+    }
+
+    public Agent createAgent(Agent a){
+        return agentRepository.save(a);
+
+    }
+
+    public Optional<Agent> getAgentByID(int id){
+        return Optional.ofNullable(agentRepository.getAgentByID(id));
     }
 }
 
