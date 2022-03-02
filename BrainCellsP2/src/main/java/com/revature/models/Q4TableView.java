fa26405d5b3d036b4394bc17ae52fb30175343a1
colleenl <[lynch.colleen@gmail.com]>
2022-03-02 11:35:10 -0500

    adding q4table adjustments

--- BrainCellsP2/src/main/java/com/revature/models/Q4TableView.java
+++ BrainCellsP2/src/main/java/com/revature/models/Q4TableView.java
@@ -6,21 +6,25 @@ import javax.persistence.*;
 
 
 @Entity
-@Table(name="q4_table")
+@Table(name="q4table")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Q4TableView {
-    @Id
-    @Column(name= "claim_id")
-    private Integer claimId;
+//    @Id
+//    @Column(name= "claim_id")
+//    private Integer claimId;
 
+    @Id
     @Column(name= "state")
     private String state;
 
-    @Column(name= "reason")
+    @Column(name= "substring")
     private String reason;
+
+    @Column(name= "num_claims")
+    private Integer numClaims;
 }
 
